e7ed6c7295bb3f1d26a8dd717d273e7655fa5947
Mohit Narayan Shah <mohit_2051@yahoo.com>
2022-06-21 15:11:55 +0100

    Code designed with multiple c++ files

---
+++ Ep005/source/include/dog.h
@@ -0,0 +1,30 @@
+#ifndef DOG_H
+#define D0G_H
+
+#include<iostream>
+#include<string>
+
+class Dog
+{
+    public:
+        explicit Dog(std::string name_param);
+        Dog() = default;
+        ~Dog();
+
+        std::string get_name() const{
+            return dog_name;
+        }
+
+        void set_dog_name(const std::string & name){
+            dog_name = name;
+        }
+
+        void print_info(){
+            std::cout << "Dog [ name : " << dog_name << " ]" <<std::endl;
+        }
+
+    private:
+    std::string dog_name {"Puffy"};
+};
+
+#endif
